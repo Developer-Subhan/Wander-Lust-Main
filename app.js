06ab378b726c14818f101f54552483146c2a5723
Developer-Subhan <mirzasubhan1010@gmail.com>
2025-08-29 19:36:40 +0500

    Fixed bugs

--- app.js
+++ app.js
@@ -105,12 +105,11 @@ app.use("/", (req, res, next) => {
 //ERROR HANDLER
 app.use((err, req, res, next) => {
   console.log(err)
-  // let { status = 500, message = "Some error occured" } = err;
-  // if(message == "Cannot read properties of undefined (reading 'geometry')") {
-  //   message = "Invalid Location";
-  // }
-  // res.status(status).render("error.ejs", { message });
-  res.send(err);
+  let { status = 500, message = "Some error occured" } = err;
+  if(message == "Cannot read properties of undefined (reading 'geometry')") {
+    message = "Invalid Location";
+  }
+  res.status(status).render("error.ejs", { message });
 });
 
 
