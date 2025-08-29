ec836c06d7e959ed41bf1553a364596e25a6c12b
Developer-Subhan <mirzasubhan1010@gmail.com>
2025-08-29 20:59:08 +0500

    Fixed bugs

--- init/index.js
+++ init/index.js
@@ -16,7 +16,7 @@ main().then(() => {
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj)=>({...obj, owner: '689f6bb68c6887da3986c748'}))
+    initData.data = initData.data.map((obj)=>({...obj, owner: '689f6bb68c6887da3986c748'}))//change userobjectID accordingly
     await Listing.insertMany(initData.data);
     console.log('Data was initialized');
 };
