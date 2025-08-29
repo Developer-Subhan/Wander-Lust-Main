2ada91f38ffae6a09764ca8a0c211fb159d3fff3
Developer-Subhan <mirzasubhan1010@gmail.com>
2025-08-29 18:53:39 +0500

    Fixed bugs

--- public/js/map.js
+++ public/js/map.js
@@ -57,5 +57,4 @@ geojson.features.forEach((marker) => {
     el.addEventListener('mouseleave', () => popup.remove());
 });
 
-console.log("Listing object:", listing);
-console.log("Coordinates:", listing.geometry?.coordinates);
+
